e50f58305a4485aa03efb52eec3b7a63199327ed
surajto <surajtomar9309@gmail.com>
2025-04-13 06:39:00 +0530

    initial setup

---
+++ src/test/java/io/github/surajto/skillhubbartersystemteamfusion/SkillhubBarterSystemTeamFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.surajto.skillhubbartersystemteamfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillhubBarterSystemTeamFusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
