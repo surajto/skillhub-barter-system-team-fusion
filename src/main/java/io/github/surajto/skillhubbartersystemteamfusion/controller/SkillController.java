065176742a2558613b27516a68a9647d5040cf68
surajto <surajtomar9309@gmail.com>
2025-04-13 16:54:11 +0530

    created a basic SkillController for skill

---
+++ src/main/java/io/github/surajto/skillhubbartersystemteamfusion/controller/SkillController.java
@@ -0,0 +1,11 @@
+package io.github.surajto.skillhubbartersystemteamfusion.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/skills")
+public class SkillController {
+
+}
