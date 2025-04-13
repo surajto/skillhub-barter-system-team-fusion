1cdd845a188a30674db7db5806e8f6a09f97a68a
surajto <surajtomar9309@gmail.com>
2025-04-13 07:26:57 +0530

    created barterRequest pojo

---
+++ src/main/java/io/github/surajto/skillhubbartersystemteamfusion/model/BarterRequest.java
@@ -0,0 +1,37 @@
+package io.github.surajto.skillhubbartersystemteamfusion.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "barter")
+public class BarterRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Unique foreign key for sender
+     */
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Student sender;
+
+    /**
+     * Unique foreign key for receiver
+     */
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private Student receiver;
+
+    /**
+     *  Unique foreign key for skill
+     */
+    @ManyToOne
+    @JoinColumn(name = "skill_id")
+    private Skills skill;
+
+    private String status;
+
+    private String message;
+}
